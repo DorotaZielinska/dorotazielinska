89a98d1ac5e254ed76a34747409e87da77e8042d
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-12-09 21:00:02 +0100

    added burger with onClick & styled for it

---
+++ src/core/TopBar/Burger/burger.js
@@ -0,0 +1,11 @@
+import { StyledBurger } from "./styled";
+
+export const Burger = ({ open, setOpen }) => {
+  return (
+    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+      <div />
+      <div />
+      <div />
+    </StyledBurger>
+  );
+};
